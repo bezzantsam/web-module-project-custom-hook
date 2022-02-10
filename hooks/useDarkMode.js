e89de1b3721200ba63fb4e9fbf2bb40bf23a6f6f
bezzantsam <83626945+bezzantsam@users.noreply.github.com>
2022-02-09 16:03:47 -0800

    useDarkMode hook complete and linked, state changed to useDark

---
+++ hooks/useDarkMode.js
@@ -0,0 +1,7 @@
+import useLocalStorage from "./useLocalStorage";
+
+const useDarkMode = (initialValue) => {
+  const [darkMode, setDarkMode] = useLocalStorage(initialValue);
+  return [darkMode, setDarkMode];
+};
+export default useDarkMode;
